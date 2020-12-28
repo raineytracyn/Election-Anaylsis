664fb70c429819aca7dde30a80a23cfb5bc2a091
raineytracyn <rainey.tracyn@gmail.com>
2020-12-28 01:54:24 -0500

    Update

---
+++ Election_Analysis/Python_practice.py
@@ -0,0 +1,5 @@
+counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+message_to_candidate = (
+    f"You received {candidate_votes:,} number of votes. "
+    f"The total number of votes in the election was {total_votes:,}. "
+    f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
